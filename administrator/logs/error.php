281463d92c8d9d5c9b0880baacf71fdc010b54a6
Alejandro <AlejandroVegaL@users.noreply.github.com>
2019-01-31 22:36:14 -0600

    portfolio

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-31 17:22:23 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-31T17:22:23+00:00	INFO ::1	joomlafailure	Empty password not allowed.
